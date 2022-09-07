2387ff6a30a2f912b1546a100547bd96ee992e98
Marcos_Mera <rogeliomera565@gmail.com>
2022-09-07 16:33:22 -0500

    Corrección al agregar productos

--- Shop Technologies/script.js
+++ Shop Technologies/script.js
@@ -1,40 +1,96 @@
 const URL = "./main.json";
+let root = document.getElementById("root");
+let carrito = [];
+console.log(URL);
 
 fetch(URL)
     .then(response => response.json())
     .then(json =>{
         
-        json.forEach(element => {
-            story.innerHTML += createElement(elemento);
+        json.forEach(elemento => {
+            root.innerHTML += createElement(elemento);
         });
+
+        eventos(json); 
     })
 
 const createElement =(elemento) =>{
     return `
-    <div class="carta">
-    <p>${elemento.nombre}</p>
+    <div class = "d-flex flex-column contenedor">
+        <div class="p-1" style ="background: ${elemento.background}">
+            <img src="${elemento.imagen}">
+        </div>
+        <h2 class="fw-bold d-flex justify-content-between py-2">${elemento.nombre}<button id="gustoss-producto"><img src="https://img.icons8.com/cotton/64/000000/like--v5.png"/></button></h2>
+        <div>${elemento.descripcion}</div>
+        <div class ="fw-bold d-flex justify-content-between py-2">$${elemento.precio}<button class="button" id="agregar-producto" href="#"><img src="https://img.icons8.com/ios-glyphs/30/000000/filled-plus-2-math.png"/></button></div>
     </div>
     `
 }
 
-let main = document.getElementById("root");
-
-const card = (nombre, descripcion, precio, img, background)=>`
-    <div class = "d-flex flex-column contenedor">
-        <div class="p-1" style ="background: ${background}">
-            <img src="${img}">
+const nuevaCompra = (nuevoselemento) =>{
+    return`
+    <div class = "d-flex contenedor w-100">
+        <div class="p-1 h-50" style ="background: ${nuevoselemento.background}">
+            <img src="${nuevoselemento.imagen}">
         </div>
-        <h2 class="fw-bold d-flex justify-content-between py-2">${nombre}<a href="#"><img src="https://img.icons8.com/cotton/64/000000/like--v5.png"/></a></h2>
-        <div>${descripcion}</div>
-        <div class ="fw-bold d-flex justify-content-between py-2">$${precio}<a id="agregar" href="#"><img src="https://img.icons8.com/ios-glyphs/30/000000/filled-plus-2-math.png"/></a></div>
+        <div class="d-flex flex-column p-2">
+            <h2 class="fw-bold d-flex justify-content-between py-2">${nuevoselemento.nombre}</h2>
+            <div>${nuevoselemento.descripcion}</div>
+        </div>
+        <button class="button" id="agregar-producto" href="#"><img src="https://img.icons8.com/ios-glyphs/30/000000/filled-plus-2-math.png"/></button>
+        <button class="button" id="agregar-producto" href="#"><img src="https://img.icons8.com/ios-glyphs/30/000000/filled-minus-2-math.png"/></button>
+        <h1 class ="fw-bold d-flex justify-content-between py-2">$${nuevoselemento.precio}</h1>
     </div>
-`
-    main.innerHTML+=card("VR Glasses CR-103","The new vr glasses are amazing if you want...",128,"./images/VR.png", "#39C7A5");
-    main.innerHTML+=card("VR Glasses CR-103","The new vr glasses are amazing if you want...",128,"./images/MN643.png","#FDE1E6");
-    main.innerHTML+=card("VR Glasses CR-103","The new vr glasses are amazing if you want...",128,"./images/PS4.png","#D5D5E0");
-    main.innerHTML+=card("VR Glasses CR-103","The new vr glasses are amazing if you want...",128,"./images/VR.png", "#39C7A5");
-    main.innerHTML+=card("VR Glasses CR-103","The new vr glasses are amazing if you want...",128,"./images/MN643.png","#FDE1E6");
-    main.innerHTML+=card("VR Glasses CR-103","The new vr glasses are amazing if you want...",128,"./images/PS4.png","#D5D5E0");
+    `
+}
+
+const pagarproducto = (pagodeproductos) =>{
+    return `
+    <div>
+        <p class="text-center">Have a coupon code? enter here</p>
+        <input class="w-100" placeholder="Your coupon">
+        <button class="btn btn-success">Continue to payment</button>
+    </div>
+    `
+}
+
+const eventos = (arr) => {
+    let buttons = document.querySelectorAll(".button");
+    buttons.forEach((button, index) =>{
+        button.addEventListener("click", () =>{
+            carrito.push(arr[index]);
+            let newElement = document.getElementById("story");
+            newElement.innerHTML = "";
+            carrito.forEach(nuevoselemento => {
+                newElement.innerHTML += nuevaCompra(nuevoselemento);
+            })
+        })
+    })
+}
+
+const paypa = (arra) =>{
+    let suma = document.quer
+}
+
+
+// let main = document.getElementById("root");
+
+// const card = (nombre, descripcion, precio, img, background)=>`
+//     <div class = "d-flex flex-column contenedor">
+//         <div class="p-1" style ="background: ${background}">
+//             <img src="${img}">
+//         </div>
+//         <h2 class="fw-bold d-flex justify-content-between py-2">${nombre}<a href="#"><img src="https://img.icons8.com/cotton/64/000000/like--v5.png"/></a></h2>
+//         <div>${descripcion}</div>
+//         <div class ="fw-bold d-flex justify-content-between py-2">$${precio}<a id="agregar" href="#"><img src="https://img.icons8.com/ios-glyphs/30/000000/filled-plus-2-math.png"/></a></div>
+//     </div>
+// `
+//     main.innerHTML+=card("VR Glasses CR-103","The new vr glasses are amazing if you want...",128,"./images/VR.png", "#39C7A5");
+//     main.innerHTML+=card("VR Glasses CR-103","The new vr glasses are amazing if you want...",128,"./images/MN643.png","#FDE1E6");
+//     main.innerHTML+=card("VR Glasses CR-103","The new vr glasses are amazing if you want...",128,"./images/PS4.png","#D5D5E0");
+//     main.innerHTML+=card("VR Glasses CR-103","The new vr glasses are amazing if you want...",128,"./images/VR.png", "#39C7A5");
+//     main.innerHTML+=card("VR Glasses CR-103","The new vr glasses are amazing if you want...",128,"./images/MN643.png","#FDE1E6");
+//     main.innerHTML+=card("VR Glasses CR-103","The new vr glasses are amazing if you want...",128,"./images/PS4.png","#D5D5E0");
 
     // let section = document.querySelector("#story");
 
